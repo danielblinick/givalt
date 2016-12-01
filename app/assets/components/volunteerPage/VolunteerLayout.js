a6d2dae1defb72a5799ef9f8535173fc27fee882
danielblinick <daniel.b@cm-studios.com>
2016-12-01 20:55:46 +0200

    map calendar correct size

--- app/assets/components/volunteerPage/VolunteerLayout.js
+++ app/assets/components/volunteerPage/VolunteerLayout.js
@@ -6,8 +6,12 @@ import styles from "./volunteerPage.scss"
 export default class VolunteerLayout extends React.Component {
 	render() {
 		return (
-			<div id='container'>
-				<Calendar></Calendar>
+			<div class='container'>
+				<div class='row'>
+					<div class='col-md-8 col-md-offset-2'>
+						<Calendar></Calendar>
+					</div>
+				</div>
 			</div>
 			);
 	}
